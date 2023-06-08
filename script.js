51f7a74bfd5c28037d624094f55a97503b9e908c
Alvaro3c <alvaro.cuallado.g@gmail.com>
2023-06-08 10:50:56 +0200

    fix/clean-code

--- script.js
+++ script.js
@@ -6,17 +6,27 @@ const patronusSelect = document.getElementById('patronus-select');
 const wandSelect = document.getElementById('wand-select');
 const inputSearch = document.getElementById('search-bar');
 const cards = document.querySelectorAll('.card');
+const form = document.querySelector('form');
 
 let arrayOfWands = [];
 let arrayOfPatronus = [];
 
 let dataArray = [];
 
-async function getData() {
+async function getDataFromCharacters() {
     try {
         const response = await fetch('https://hp-api.onrender.com/api/characters');
         const data = await response.json();
         dataArray = [...data];
+        data.forEach(character => {
+            if (arrayOfWands.indexOf(character.wand.core) === -1 && character.wand.core !== "") {
+                arrayOfWands.push(character.wand.core);
+            };
+
+            if (arrayOfPatronus.indexOf(character.patronus) === -1 && character.patronus !== "") {
+                arrayOfPatronus.push(character.patronus);
+            };
+        })
         return data;
     } catch (error) {
         console.log(error);
@@ -24,64 +34,55 @@ async function getData() {
     }
 }
 
-async function getCharacters(data) {
-    function printCharacters() {
-        charactersContainer.innerHTML = '';
-        for (let i = 0; i < data.length; i++) {
-            let cardCharacter = document.createElement('article');
-            cardCharacter.setAttribute('class', `card ${data[i].house} `);
-            charactersContainer.appendChild(cardCharacter);
-            cardCharacter.innerHTML =
-                `<img src="${data[i].image}" alt="">
-                <h3>${data[i].name}</h3>
-                <div class="character-data"
-                    <label">Species: ${data[i].species}</label>
-                    <label id="label-house">House: ${data[i].house}</label>
-                    <label id="label-patronus" class ="neon-text">Patronus: ${data[i].patronus}</label>
-                    <label id="label-wand-core" >wand: ${data[i].wand.core}</label>
-                    <label><a href="characters-full.html">More details</a></label>
-                </div>
-                `
-            //return an array with no repeated elements of wand's core //CHECK BIEN
-            if (arrayOfWands.indexOf(data[i].wand.core) === -1 && data[i].wand.core !== "") {
-                arrayOfWands.push(data[i].wand.core);
-            };
-
-            //return an array with no repeated elements of patronus //CHECK BIEN
-            if (arrayOfPatronus.indexOf(data[i].patronus) === -1 && data[i].patronus !== "") {
-                arrayOfPatronus.push(data[i].patronus);
-            };
-        };
-        return arrayOfPatronus, arrayOfWands
-    };
-    function printPatronusOptions() {
-        const fragment = document.createDocumentFragment();
-        for (patronus of arrayOfPatronus) {
-            const option = document.createElement('option')
-            option.value = patronus;
-            option.textContent = patronus;
-            fragment.appendChild(option);
-        }
-
-        patronusSelect.appendChild(fragment);
+function printPatronusOptions() {
+    for (patronus of arrayOfPatronus) {
+        const option = document.createElement('option')
+        option.value = patronus;
+        option.textContent = patronus;
+        patronusSelect.appendChild(option);
     }
-    function printWandOptions() {
-        for (wand of arrayOfWands) {
-            wandSelect.innerHTML += `<option value="${wand}">${wand}</option>`
-        }
-    }
-    printCharacters();
-    printWandOptions()
-    printPatronusOptions()
 }
 
-function filterCharacters(event) {
-    console.log(event);
+function printWandOptions() {
+    for (wand of arrayOfWands) {
+        const option = document.createElement('option')
+        option.value = wand;
+        option.textContent = wand;
+        wandSelect.appendChild(option);
+    }
+}
+
+function printCharacters(data) {
+    charactersContainer.innerHTML = '';
+    for (let i = 0; i < data.length; i++) {
+        let cardCharacter = document.createElement('article');
+        cardCharacter.setAttribute('class', `card ${data[i].house} `);
+        charactersContainer.appendChild(cardCharacter);
+        cardCharacter.innerHTML =
+            `<img src="${data[i].image}" alt="">
+            <h3>${data[i].name}</h3>
+            <div class="character-data"
+                <label">Species: ${data[i].species}</label>
+                <label id="label-house">House: ${data[i].house}</label>
+                <label id="label-patronus" class ="neon-text">Patronus: ${data[i].patronus}</label>
+                <label id="label-wand-core" >wand: ${data[i].wand.core}</label>
+                <label><a href="characters-full.html">More details</a></label>
+            </div>
+            `
+    };
+};
+
+async function getCharacters(data) {
+    printWandOptions()
+    printPatronusOptions()
+    printCharacters(data);
+}
+
+function filterCharacters() {
     const search = inputSearch.value.toLowerCase();
     const house = houseSelect.value.toLowerCase();
     const patronus = patronusSelect.value.toLowerCase();
     const wand = wandSelect.value.toLowerCase();
-    console.log(house);
 
     const formattedData = dataArray.filter(character => {
         const nameMatch = !search || (search && character.name.toLowerCase().includes(search));
@@ -89,7 +90,7 @@ function filterCharacters(event) {
         const patronusMatch = !patronus || (patronus && character.patronus.toLowerCase().includes(patronus));
         const wandMatch = !wand || (wand && character.wand.core.toLowerCase().includes(wand));
 
-        console.log(nameMatch, houseMatch, patronusMatch, wandMatch);
+
         return nameMatch && houseMatch && patronusMatch && wandMatch;
     })
 
@@ -101,19 +102,17 @@ function filterCharacters(event) {
 }
 
 async function initialize() {
-    const data = await getData();
+    const data = await getDataFromCharacters();
     getCharacters(data);
-    document.querySelector('form').addEventListener('input', filterCharacters);
+    form.addEventListener('input', filterCharacters);
 }
-
-initialize()
+initialize();
 
 async function getAndPrintSpells() {
     let response = await fetch('https://hp-api.onrender.com/api/spells');
     let data = await response.json();
     //Print Characters
     for (let i = 0; i < data.length; i++) {
-
         let cardSpells = document.createElement('article');
         cardSpells.setAttribute('class', 'card spells');
         spellsContainer.appendChild(cardSpells)
